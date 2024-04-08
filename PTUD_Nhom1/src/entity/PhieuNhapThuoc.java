ea6266018a44c54934057f21e364b46da383e082
Văn Vĩ <88929565+VanVixZ@users.noreply.github.com>
2024-04-09 03:41:23 +0700

    Update PhieuNhapThuoc.java
    sua pnt

--- PTUD_Nhom1/src/entity/PhieuNhapThuoc.java
+++ PTUD_Nhom1/src/entity/PhieuNhapThuoc.java
@@ -7,79 +7,62 @@ public class PhieuNhapThuoc {
 	private String maPhieuNhap;
 	private String maNCC;
 	private String maNV;
-	private ChiTietPhieuNhapThuoc chiTietPhieuNhapThuoc;
+	private LocalDate ngayNhap;
+	public LocalDate getNgayNhap() {
+		return ngayNhap;
+	}
+	public void setNgayNhap(LocalDate ngayNhap) {
+		this.ngayNhap = ngayNhap;
+	}
 	private Double tongTien;
 	private Boolean trangThai;
-
 	public String getMaPhieuNhap() {
 		return maPhieuNhap;
 	}
-
 	public void setMaPhieuNhap(String maPhieuNhap) {
 		this.maPhieuNhap = maPhieuNhap;
 	}
-
 	public String getMaNCC() {
 		return maNCC;
 	}
-
 	public void setMaNCC(String maNCC) {
 		this.maNCC = maNCC;
 	}
-
 	public String getMaNV() {
 		return maNV;
 	}
-
 	public void setMaNV(String maNV) {
 		this.maNV = maNV;
 	}
-
-	public ChiTietPhieuNhapThuoc getChiTietPhieuNhapThuoc() {
-		return chiTietPhieuNhapThuoc;
-	}
-
-	public void setChiTietPhieuNhapThuoc(ChiTietPhieuNhapThuoc chiTietPhieuNhapThuoc) {
-		this.chiTietPhieuNhapThuoc = chiTietPhieuNhapThuoc;
-	}
-
+	
 	public Double getTongTien() {
 		return tongTien;
 	}
-
 	public void setTongTien(Double tongTien) {
 		this.tongTien = tongTien;
 	}
-
 	public Boolean getTrangThai() {
 		return trangThai;
 	}
-
 	public void setTrangThai(Boolean trangThai) {
 		this.trangThai = trangThai;
 	}
-
-	public PhieuNhapThuoc(String maPhieuNhap, String maNCC, String maNV, ChiTietPhieuNhapThuoc chiTietPhieuNhapThuoc,
-			Double tongTien, Boolean trangThai) {
+	public PhieuNhapThuoc(String maPhieuNhap, String maNCC, String maNV, LocalDate ngayNhap, Double tongTien,
+			Boolean trangThai) {
 		super();
 		this.maPhieuNhap = maPhieuNhap;
 		this.maNCC = maNCC;
 		this.maNV = maNV;
-		this.chiTietPhieuNhapThuoc = chiTietPhieuNhapThuoc;
+		this.ngayNhap = ngayNhap;
 		this.tongTien = tongTien;
 		this.trangThai = trangThai;
 	}
-
+	
 	public PhieuNhapThuoc() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
-
-	@Override
-	public String toString() {
-		return "PhieuNhapThuoc [maPhieuNhap=" + maPhieuNhap + ", maNCC=" + maNCC + ", maNV=" + maNV
-				+ ", chiTietPhieuNhapThuoc=" + chiTietPhieuNhapThuoc + ", tongTien=" + tongTien + ", trangThai="
-				+ trangThai + "]";
-	}
+	
+	
+	
 
 }
